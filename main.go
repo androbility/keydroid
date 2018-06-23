d3655e7e31ecd4ee8e6586cf965c5e9d58961d6c
Steven Edwards <steven@stephenwithav.com>
2018-06-23 16:34:34 -0400

    Stable h, j, k, l, i, x, q.

--- main.go
+++ main.go
@@ -7,20 +7,19 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/eiannone/keyboard"
-	adb "github.com/yosemite-open/go-adb"
+	"github.com/stephenwithav/keydroid/shell"
 )
 
 func main() {
-	_, err := adb.New()
+	cmd, stdin, stdout, err := shell.New()
 	if err != nil {
 		log.Fatalf("error connecting to adb server: %s\n\nDid you run adb connect?\n", err)
 	}
+	defer stdin.Close()
+	defer stdout.Close()
 
-	if err := keyboard.Open(); err != nil {
-		log.Fatal(err)
-	}
-	defer keyboard.Close()
-	stdin := bufio.NewScanner(bufio.NewReader(os.Stdin))
+	// For keyboard input
+	scanIn := bufio.NewScanner(bufio.NewReader(os.Stdin))
 
 	for {
 		ch, _, err := keyboard.GetSingleKey()
@@ -31,19 +30,50 @@ func main() {
 		switch ch {
 		case 'H', 'h':
 			log.Info("Move left")
+			_, err := stdin.Write([]byte("input keyevent KEYCODE_DPAD_LEFT\n"))
+			if err != nil {
+				log.Errorf("err: %s")
+			}
 		case 'J', 'j':
-			log.Info("Move up")
+			log.Info("Move down")
+			_, err := stdin.Write([]byte("input keyevent KEYCODE_DPAD_DOWN\n"))
+			if err != nil {
+				log.Errorf("err: %s")
+			}
 		case 'K', 'k':
 			log.Info("Move down")
+			_, err := stdin.Write([]byte("input keyevent KEYCODE_DPAD_UP\n"))
+			if err != nil {
+				log.Errorf("err: %s")
+			}
 		case 'L', 'l':
 			log.Info("Move right")
+			_, err := stdin.Write([]byte("input keyevent KEYCODE_DPAD_RIGHT\n"))
+			if err != nil {
+				log.Errorf("err: %s")
+			}
 		case 'I', 'i':
 			log.Info("Begin input")
 			fmt.Print("> ")
-			stdin.Scan()
-			log.Infof("You entered: %s", stdin.Text())
+			scanIn.Scan()
+			log.Infof("You entered: %s", scanIn.Text())
+			_, err := stdin.Write([]byte(scanIn.Text()))
+			if err != nil {
+				log.Errorf("err: %s")
+			}
+		case 'X', 'x':
+			log.Info("Pressed Enter")
+			_, err := stdin.Write([]byte("input keyevent KEYCODE_ENTER\n"))
+			if err != nil {
+				log.Errorf("err: %s")
+			}
 		case 'Q', 'q':
 			log.Info("Quitting")
+			_, err := stdin.Write([]byte("exit\n"))
+			if err != nil {
+				log.Errorf("err: %s")
+			}
+			cmd.Wait()
 			os.Exit(0)
 		}
 	}
