80e7d492d176e2ece569e9b336b08b774d257b1f
Steven Edwards <steven@stephenwithav.com>
2018-06-27 16:34:54 -0400

    Integrate adbcmd with keydroid; add default keybindings for kodikeys.

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 )
 
 func main() {
+	adbcmd.LoadConfigFile("$HOME/.keydroid", defaultBindings)
 	for {
 		adbcmd.WaitForAndroid()
 		err := Watch()
@@ -45,3 +46,38 @@ func Watch() error {
 		}
 	}
 }
+
+// Default keybindings for keydroid.
+var defaultBindings = `{
+	"keybindings": {
+		"\b":   "KEYCODE_BACK",
+		"\t":   "KEYCODE_TAB",
+		"\r":   "KEYCODE_ENTER",
+		" ":    "KEYCODE_SPACE",
+		"-":    "KEYCODE_VOLUME_DOWN",
+		"=":    "KEYCODE_VOLUME_UP",
+		"+":    "KEYCODE_VOLUME_UP",
+		"1":    "KEYCODE_TV_INPUT_HDMI_1",
+		"2":    "KEYCODE_TV_INPUT_HDMI_2",
+		"3":    "KEYCODE_TV_INPUT_HDMI_3",
+		"4":    "KEYCODE_TV_INPUT_HDMI_4",
+		"a":    "KEYCODE_ASSIST",
+		"b":    "KEYCODE_BACK",
+		"c":    "KEYCODE_DPAD_CENTER",
+		"f":    "KEYCODE_MEDIA_FAST_FORWARD",
+		"h":    "KEYCODE_DPAD_LEFT",
+		"i":    "KEYCODE_TV_INPUT",
+		"j":    "KEYCODE_DPAD_DOWN",
+		"k":    "KEYCODE_DPAD_UP",
+		"l":    "KEYCODE_DPAD_RIGHT",
+		"m":    "KEYCODE_MUTE",
+		"o":    "KEYCODE_HOME",
+		"r":    "KEYCODE_MEDIA_REWIND",
+		"s":    "KEYCODE_MEDIA_STOP",
+		"t":    "KEYCODE_TV_TIMER_PROGRAMMING",
+		"u":    "KEYCODE_MENU",
+		"v":    "KEYCODE_VOICE_ASSIST",
+		"w":    "KEYCODE_WAKEUP"
+	}
+}
+`
